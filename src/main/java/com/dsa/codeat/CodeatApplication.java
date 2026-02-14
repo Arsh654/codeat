41863b91c02e38f962fa0782d9fe5c44bcc6d500
Arsh654 <arshadali8376@gmail.com>
2026-02-14 11:48:40 +0530

    Initial commit

---
+++ src/main/java/com/dsa/codeat/CodeatApplication.java
@@ -0,0 +1,13 @@
+package com.dsa.codeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeatApplication.class, args);
+	}
+
+}
